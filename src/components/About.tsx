edb7c9fa42b0800f64332b83808915821615951c
thanhton115 <thanhtontran115@gmail.com>
2026-05-16 23:27:56 +0700

    refactor/about-home-technical: enhance structure and content in About and Home components
    - Update About component to include new CV link and improved descriptions
    - Refactor Home component to use list elements for descriptions
    - Adjust Technical component to improve layout of technology items
    - Modify types.tsx to update path and title for navigation items
    - Clean up TimelineTree component for better modal content handling

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,19 +1,14 @@
 import Image from "next/image";
 import Avatar from "@image/avatar.jpg";
+import Link from "next/link";
 
-const CvUrl =
-  "https://drive.google.com/file/d/15A1eWRg3iAvTJ1LwAItPhFeIgcEMuV-1/view?usp=sharing";
 export default function About() {
   return (
     <section id="about" className="pt-[74px]">
       <h2 className="text-3xl text-center w-full font-bold">About me</h2>
       <h3 className="text-[--label] text-center w-full">My introduction</h3>
       <div className="flex flex-wrap mt-16 gap-16 justify-center items-start">
-        <Image
-          className="about__img w-[300px]"
-          src={Avatar}
-          alt="avatar"
-        />
+        <Image className="about__img w-[300px]" src={Avatar} alt="avatar" />
         <div className="about__info max-md:w-full md:max-w-[560px]  max-md:text-center">
           <div className="grid gap-4 grid-cols-2 mb-4">
             <div className="about__card border border-[--label] rounded-lg p-4 text-center">
@@ -25,28 +20,34 @@ export default function About() {
               <p className="text-[--label]">6 projects</p>
             </div>
           </div>
-          <div className="flex flex-col gap-2">
-            <p className="text-[--label]  text-justify">
-              Frontend Developer with 4+ years of experience building scalable
-              and maintainable web applications.
-            </p>
-            <p className="text-[--label]  text-justify">
-              Specializing in React and Next.js, with strong experience in
-              building production-ready, scalable frontend systems. Previously
-              worked extensively with Vue.js (Vue 2 / Vue 3).
-            </p>
-            <p className="text-[--label]  text-justify">
-              Experienced in frontend architecture design, Design Systems, and
-              performance optimization for complex product environments.
-            </p>
-            <a
-              href={CvUrl}
+          <ul className="flex flex-col gap-2">
+            <li className="text-[--label]  text-justify">
+              Frontend Engineer with 4+ years of experience building scalable
+              and maintainable web applications using React, Next.js, and modern
+              frontend technologies, with a strong focus on reusable component
+              systems, scalable frontend architecture, UI consistency,
+              performance optimization and complex business platforms including
+              ERP, HRM, CRM, and internal management systems.
+            </li>
+            <li className="text-[--label]  text-justify">
+              Skilled in collaborating closely with UX/UI designers, backend
+              engineers, and product teams to deliver responsive and
+              user-centric web applications aligned with business requirements.
+            </li>
+            <li className="text-[--label]  text-justify">
+              Previously worked extensively with Vue.js ecosystems (Vue 2 / Vue
+              3) before transitioning into React-focused product development
+              using TypeScript, modern state management, and scalable frontend
+              patterns.
+            </li>
+            <Link
+              href={process.env.NEXT_PUBLIC_CV_PATH || ""}
               target="_blank"
               className="bg-[var(--secondary)] text-white rounded-full px-6 py-4 w-fit"
             >
               View CV
-            </a>
-          </div>
+            </Link>
+          </ul>
         </div>
       </div>
     </section>
